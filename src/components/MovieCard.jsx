9059a8e6cbb15384a9c0b0c6814dfd5e5e695c9a
seongbeen jeon <wjstjdqls98@gmail.com>
2026-04-24 17:07:32 +0900

    무한 스크롤 추가

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,9 @@ import {Link} from 'react-router-dom';
 
 function MovieCard({id, title,posterPath}){
   return(
-    <div className="MovieCard shrink-0 w-50">
-      <Link className="w-full"to={`/detail/${id}`}><img src={`https://image.tmdb.org/t/p/w200${posterPath}`} /></Link>
-      <span className="w-full">{title}</span>
+    <div className="MovieCard shrink-0 w-50 ">
+      <Link className="w-full"to={`/detail/${id}`}><img className="rounded-4xl"src={`https://image.tmdb.org/t/p/w200${posterPath}`} /></Link>
+      <div className="w-full">{title}</div>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,51 +1,70 @@
 import MovieCard from '../components/MovieCard.jsx';
 import SearchBar from '../components/SearchBar.jsx';
-import {useState} from 'react';
-import {useEffect} from 'react';
-
+import {useState,useEffect,useRef} from 'react';
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const [pages,setPages] = useState(1);
+  const scrollTrigger = useRef(null);
+
+  const url = import.meta.env.VITE_API_URL;
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json'
+    }
+  };
 
   useEffect(()=>{
     async function loadMovie(){
       try{
-        const url = import.meta.env.VITE_API_URL;
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json'
-          }
-        };
-        const response = await fetch(`${url}/api/movies`,options);
+
+        const response = await fetch(`${url}/api/movies?page=${pages}`,options);
         
         if(!response.ok){
           throw new Error("network error");
         }
 
         const json = await response.json();
-
-        setMovies(json.results);
+        console.log(pages);
+        setMovies(prev=>[...prev, ...json.results]);
       }catch(error){
         console.log(error);
-      }finally{
-        setLoading(false);
       }
+      setLoading(false);
+      
     }
     loadMovie();
-  },[]);
+  },[pages]);
+
+  useEffect(()=>{
+    const observer = new IntersectionObserver((entries)=>{
+      if(entries[0].isIntersecting && !loading){
+        setPages(prev=>prev+1);
+      }
+    });
+
+    if(scrollTrigger.current){
+      observer.observe(scrollTrigger.current);
+    }
+    
+    return () => observer.disconnect();
+  },[loading])
+
 
   if(loading) return <h1>Loading... </h1>;
   return ( 
     <>
     <SearchBar/>
-    <div id="MovieList" className="flex *:p-5 size overflow-x-auto">
+    <div id="MovieList" className="*:p-5 grid grid-cols-[repeat(auto-fit,minmax(180px,1fr))]">
       {
         movies.map((movie)=>
             <MovieCard key = {movie.id} id={movie.id} title={movie.title} posterPath={movie.poster_path}/>
         )
       }
+      <div ref={scrollTrigger}></div>
     </div>
     </>
   )
